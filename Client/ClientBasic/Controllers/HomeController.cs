9be49456f45c2211d4c9768f387ce1a64302ef7b
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-04-24 13:48:51 +0300

    UpdateProduct Methodu Oluşturuldu

--- Client/ClientBasic/Controllers/HomeController.cs
+++ Client/ClientBasic/Controllers/HomeController.cs
@@ -41,10 +41,35 @@ namespace ClientBasic.Controllers
             }
             ModelState.AddModelError("","Ekleme İşlemi Başarısız");
             return View();
-        
+      
+        }   
+
+        public IActionResult Edit(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var responseMessage = httpClient.GetAsync($"https://localhost:44388/api/Products/{id}").Result;
+            if(responseMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var product = JsonConvert.DeserializeObject<Product>(responseMessage.Content.ReadAsStringAsync().Result);
+                return View(product);
+            }
+            return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            HttpClient httpClient = new HttpClient();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8, "application/json");
+            var responseMessage = httpClient.PutAsync($"https://localhost:44388/api/Products/{product.Id}",content).Result;
+            /*
+            if(responseMessage.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
 
+            }
+            */
+            return RedirectToAction("Index");
+        }
 
     }
 }
